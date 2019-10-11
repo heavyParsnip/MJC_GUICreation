54a35092753ce91406df51ae0350fcd5fc31adf5
Matthew Camera <mjc9536@rit.edu>
2019-10-11 13:30:28 -0400

    UI Events PE completed

--- MJC_GUICreation/Form1.Designer.cs
+++ MJC_GUICreation/Form1.Designer.cs
@@ -37,8 +37,6 @@
             this.rulesLabel = new System.Windows.Forms.Label();
             this.timerBar = new System.Windows.Forms.ProgressBar();
             this.gameButton = new System.Windows.Forms.Button();
-            this.timerBox = new System.Windows.Forms.TextBox();
-            this.resultLabel = new System.Windows.Forms.Label();
             this.statusLabel = new System.Windows.Forms.Label();
             this.resultBox = new System.Windows.Forms.TextBox();
             this.redWire = new System.Windows.Forms.Button();
@@ -58,181 +56,192 @@
             this.ruleBox.Controls.Add(this.label3);
             this.ruleBox.Controls.Add(this.label2);
             this.ruleBox.Controls.Add(this.rulesLabel);
-            this.ruleBox.Location = new System.Drawing.Point(12, 12);
+            this.ruleBox.Location = new System.Drawing.Point(6, 6);
+            this.ruleBox.Margin = new System.Windows.Forms.Padding(2);
             this.ruleBox.Name = "ruleBox";
-            this.ruleBox.Size = new System.Drawing.Size(850, 272);
+            this.ruleBox.Padding = new System.Windows.Forms.Padding(2);
+            this.ruleBox.Size = new System.Drawing.Size(410, 141);
             this.ruleBox.TabIndex = 0;
             this.ruleBox.TabStop = false;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(7, 213);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 111);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(358, 31);
+            this.label5.Size = new System.Drawing.Size(169, 16);
             this.label5.TabIndex = 4;
             this.label5.Text = "-Otherwise, cut the last wire.";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(7, 172);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(843, 31);
+            this.label4.Size = new System.Drawing.Size(403, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "-Otherwise, if there is more than one blue wire, cut the last blue wire. ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(6, 131);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 68);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(644, 31);
+            this.label3.Size = new System.Drawing.Size(302, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "-Otherwise, if the last wire is white, cut the last wire. ";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(7, 90);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(4, 47);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(565, 31);
+            this.label2.Size = new System.Drawing.Size(271, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "-If there are no red wires, cut the second wire.";
             // 
             // rulesLabel
             // 
             this.rulesLabel.AutoSize = true;
-            this.rulesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rulesLabel.Location = new System.Drawing.Point(375, 27);
+            this.rulesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rulesLabel.Location = new System.Drawing.Point(188, 14);
+            this.rulesLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.rulesLabel.Name = "rulesLabel";
-            this.rulesLabel.Size = new System.Drawing.Size(124, 42);
+            this.rulesLabel.Size = new System.Drawing.Size(63, 24);
             this.rulesLabel.TabIndex = 0;
             this.rulesLabel.Text = "Rules:";
             // 
             // timerBar
             // 
-            this.timerBar.Location = new System.Drawing.Point(12, 1105);
+            this.timerBar.Location = new System.Drawing.Point(6, 495);
+            this.timerBar.Margin = new System.Windows.Forms.Padding(2);
             this.timerBar.Name = "timerBar";
-            this.timerBar.Size = new System.Drawing.Size(1178, 56);
+            this.timerBar.Size = new System.Drawing.Size(466, 29);
             this.timerBar.TabIndex = 1;
             // 
             // gameButton
             // 
-            this.gameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameButton.Location = new System.Drawing.Point(1205, 1105);
+            this.gameButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameButton.Location = new System.Drawing.Point(476, 495);
+            this.gameButton.Margin = new System.Windows.Forms.Padding(2);
             this.gameButton.Name = "gameButton";
-            this.gameButton.Size = new System.Drawing.Size(215, 56);
+            this.gameButton.Size = new System.Drawing.Size(108, 29);
             this.gameButton.TabIndex = 2;
             this.gameButton.Text = "Start Game";
             this.gameButton.UseVisualStyleBackColor = true;
-            // 
-            // timerBox
-            // 
-            this.timerBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timerBox.Location = new System.Drawing.Point(12, 1050);
-            this.timerBox.Name = "timerBox";
-            this.timerBox.Size = new System.Drawing.Size(139, 38);
-            this.timerBox.TabIndex = 3;
-            // 
-            // resultLabel
-            // 
-            this.resultLabel.AutoSize = true;
-            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultLabel.Location = new System.Drawing.Point(6, 1016);
-            this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(145, 31);
-            this.resultLabel.TabIndex = 4;
-            this.resultLabel.Text = "Time Left:";
+            this.gameButton.Click += new System.EventHandler(this.gameButton_Click);
             // 
             // statusLabel
             // 
             this.statusLabel.AutoSize = true;
-            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statusLabel.Location = new System.Drawing.Point(6, 909);
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(3, 393);
+            this.statusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(107, 31);
+            this.statusLabel.Size = new System.Drawing.Size(55, 16);
             this.statusLabel.TabIndex = 5;
             this.statusLabel.Text = "Status:";
             // 
             // resultBox
             // 
-            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.resultBox.Location = new System.Drawing.Point(12, 943);
+            this.resultBox.BackColor = System.Drawing.SystemColors.Window;
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.Location = new System.Drawing.Point(6, 410);
+            this.resultBox.Margin = new System.Windows.Forms.Padding(2);
             this.resultBox.Name = "resultBox";
-            this.resultBox.Size = new System.Drawing.Size(365, 38);
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(333, 22);
             this.resultBox.TabIndex = 6;
+            this.resultBox.TabStop = false;
             // 
             // redWire
             // 
             this.redWire.BackColor = System.Drawing.Color.Crimson;
             this.redWire.Enabled = false;
-            this.redWire.Location = new System.Drawing.Point(42, 451);
+            this.redWire.Location = new System.Drawing.Point(22, 202);
+            this.redWire.Margin = new System.Windows.Forms.Padding(2);
             this.redWire.Name = "redWire";
-            this.redWire.Size = new System.Drawing.Size(620, 40);
+            this.redWire.Size = new System.Drawing.Size(310, 21);
             this.redWire.TabIndex = 7;
             this.redWire.UseVisualStyleBackColor = false;
+            this.redWire.Click += new System.EventHandler(this.wire_Click);
             // 
             // blackWire
             // 
             this.blackWire.BackColor = System.Drawing.Color.Black;
             this.blackWire.Enabled = false;
-            this.blackWire.Location = new System.Drawing.Point(42, 501);
+            this.blackWire.Location = new System.Drawing.Point(22, 228);
+            this.blackWire.Margin = new System.Windows.Forms.Padding(2);
             this.blackWire.Name = "blackWire";
-            this.blackWire.Size = new System.Drawing.Size(620, 40);
+            this.blackWire.Size = new System.Drawing.Size(310, 21);
             this.blackWire.TabIndex = 8;
             this.blackWire.UseVisualStyleBackColor = false;
+            this.blackWire.Click += new System.EventHandler(this.wire_Click);
             // 
             // blueWire
             // 
             this.blueWire.BackColor = System.Drawing.Color.MediumBlue;
             this.blueWire.Enabled = false;
-            this.blueWire.Location = new System.Drawing.Point(42, 551);
+            this.blueWire.Location = new System.Drawing.Point(22, 254);
+            this.blueWire.Margin = new System.Windows.Forms.Padding(2);
             this.blueWire.Name = "blueWire";
-            this.blueWire.Size = new System.Drawing.Size(620, 40);
+            this.blueWire.Size = new System.Drawing.Size(310, 21);
             this.blueWire.TabIndex = 9;
             this.blueWire.UseVisualStyleBackColor = false;
+            this.blueWire.Click += new System.EventHandler(this.wire_Click);
             // 
             // whiteWire
             // 
             this.whiteWire.BackColor = System.Drawing.Color.White;
             this.whiteWire.Enabled = false;
-            this.whiteWire.Location = new System.Drawing.Point(42, 601);
+            this.whiteWire.Location = new System.Drawing.Point(22, 280);
+            this.whiteWire.Margin = new System.Windows.Forms.Padding(2);
             this.whiteWire.Name = "whiteWire";
-            this.whiteWire.Size = new System.Drawing.Size(620, 40);
+            this.whiteWire.Size = new System.Drawing.Size(310, 21);
             this.whiteWire.TabIndex = 10;
             this.whiteWire.UseVisualStyleBackColor = false;
+            this.whiteWire.Click += new System.EventHandler(this.wire_Click);
             // 
             // yellowWire
             // 
             this.yellowWire.BackColor = System.Drawing.Color.Yellow;
             this.yellowWire.Enabled = false;
-            this.yellowWire.Location = new System.Drawing.Point(42, 651);
+            this.yellowWire.Location = new System.Drawing.Point(22, 306);
+            this.yellowWire.Margin = new System.Windows.Forms.Padding(2);
             this.yellowWire.Name = "yellowWire";
-            this.yellowWire.Size = new System.Drawing.Size(620, 40);
+            this.yellowWire.Size = new System.Drawing.Size(310, 21);
             this.yellowWire.TabIndex = 11;
             this.yellowWire.UseVisualStyleBackColor = false;
+            this.yellowWire.Click += new System.EventHandler(this.wire_Click);
             // 
             // wireBox
             // 
             this.wireBox.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.wireBox.Location = new System.Drawing.Point(25, 431);
+            this.wireBox.Location = new System.Drawing.Point(13, 191);
+            this.wireBox.Margin = new System.Windows.Forms.Padding(2);
             this.wireBox.Name = "wireBox";
-            this.wireBox.Size = new System.Drawing.Size(652, 277);
+            this.wireBox.Padding = new System.Windows.Forms.Padding(2);
+            this.wireBox.Size = new System.Drawing.Size(326, 144);
             this.wireBox.TabIndex = 12;
             this.wireBox.TabStop = false;
             // 
+            // progressTimer
+            // 
+            this.progressTimer.Tick += new System.EventHandler(this.progressTimer_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1432, 1173);
+            this.ClientSize = new System.Drawing.Size(590, 529);
             this.Controls.Add(this.yellowWire);
             this.Controls.Add(this.whiteWire);
             this.Controls.Add(this.blueWire);
@@ -240,14 +249,14 @@
             this.Controls.Add(this.redWire);
             this.Controls.Add(this.resultBox);
             this.Controls.Add(this.statusLabel);
-            this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.timerBox);
             this.Controls.Add(this.gameButton);
             this.Controls.Add(this.timerBar);
             this.Controls.Add(this.ruleBox);
             this.Controls.Add(this.wireBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Do Not Explode?";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ruleBox.ResumeLayout(false);
             this.ruleBox.PerformLayout();
             this.ResumeLayout(false);
@@ -265,8 +274,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ProgressBar timerBar;
         private System.Windows.Forms.Button gameButton;
-        private System.Windows.Forms.TextBox timerBox;
-        private System.Windows.Forms.Label resultLabel;
         private System.Windows.Forms.Label statusLabel;
         private System.Windows.Forms.TextBox resultBox;
         private System.Windows.Forms.Button redWire;
--- MJC_GUICreation/Form1.cs
+++ MJC_GUICreation/Form1.cs
@@ -12,18 +12,90 @@ namespace MJC_GUICreation
 {
     public partial class Form1 : Form
     {
+        //FIELDS
+        List<Button> wires = new List<Button>();
+
+        //CONSTRUCTORS
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        //EVENTS
+        //Initialize wires
+        private void Form1_Load(object sender, EventArgs e)
         {
-
+            wires.Add(redWire);
+            wires.Add(blackWire);
+            wires.Add(blueWire);
+            wires.Add(whiteWire);
+            wires.Add(yellowWire);
         }
 
-        private void label5_Click(object sender, EventArgs e)
+        //Enable wires and timer
+        private void gameButton_Click(object sender, EventArgs e)
         {
+            //Enable all the wires, disable the start button, and clear the status box
+            foreach (Button w in wires)
+            {
+                w.Enabled = true;
+            }
+            gameButton.Enabled = false;
+            gameButton.Text = "Running...";
+            resultBox.Text = "";
+
+            //Reset and enable the timer
+            progressTimer.Enabled = true;
+            timerBar.Value = 0;
+            progressTimer.Start();
+        }
+
+        private void wire_Click(object sender, EventArgs e)
+        {
+            //Stop the timer
+            progressTimer.Stop();
+
+            //Check if the wire was the correct one
+            if((Button)sender == yellowWire)
+            {
+                resultBox.Text = "You win!";
+            }
+            else
+            {
+                resultBox.Text = "KABOOM! You lost.";
+            }
+
+            //Disable the wires & enable the reset button
+            foreach (Button w in wires)
+            {
+                w.Enabled = false;
+            }
+
+            gameButton.Text = "Reset";
+            gameButton.Enabled = true;
+        }
+
+        private void progressTimer_Tick(object sender, EventArgs e)
+        {
+            //Increment the progress bar accordingly, check if time has run out
+            if (timerBar.Value < 100)
+            {
+                timerBar.Value += 1;
+            }
+            else if(timerBar.Value == 100)
+            {
+                progressTimer.Stop();
+                resultBox.Text = "KABOOM! You lost.";
+
+                foreach (Button w in wires)
+                {
+                    w.Enabled = false;
+                }
+
+                gameButton.Text = "Reset";
+                gameButton.Enabled = true;
+
+            }
 
         }
     }
